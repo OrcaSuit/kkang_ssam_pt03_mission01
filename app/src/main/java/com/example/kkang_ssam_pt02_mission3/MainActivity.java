905fa38fb6ed8d65a558632dce68f1c17511800e
home <orcasuit@gmail.com>
2019-05-06 19:43:36 +0900

    Initial commit

---
+++ app/src/main/java/com/example/kkang_ssam_pt02_mission3/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.kkang_ssam_pt02_mission3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
